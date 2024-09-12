441cef0d59cc15191cc65837d6c86430b7108676
Alessandro Milán <alessandromilanortega@gmail.com>
2024-09-12 16:34:20 +0000

    Adding Base code to the repository.

---
+++ dictionary.c
@@ -0,0 +1,5 @@
+#include "dictionary.h"
+
+void initializeDataDictionary() {
+    printf("Initializing Data Dictionary...\n");
+}
---
+++ dictionary.h
@@ -0,0 +1,8 @@
+#ifndef DICTIONARY_H
+#define DICTIONARY_H
+
+#include <stdio.h>
+
+void initializeDataDictionary();
+
+#endif
