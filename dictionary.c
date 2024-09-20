c2f1e35193a304f016dca78d8ad0e709dd3d9b82
reginaarmz <a375925@alumnos.uaslp.mx>
2024-09-20 10:22:38 -0600

    Update dictionary.c

--- dictionary.c
+++ dictionary.c
@@ -1,5 +1,14 @@
 #include "dictionary.h"
 
-void initializeDataDictionary() {
+void initializeDataDictionary(const char *dictionary) {
+
+    long mainHeader= EMPTY_POINTE;
+    
     printf("Initializing Data Dictionary...\n");
+    FILE *dictionary = fopen(dictionary, " ");
+
+    fwrite(&mainHeader, sizeof(mainHeader), 1, dictionary);
+    fseek(dictionary, 50, SEEK_SET);
+    fwrite(&mainHeader, sizeof(mainHeader), 1, dictionary);
+    
 }
