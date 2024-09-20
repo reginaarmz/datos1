7328d08deb9adb8678c3694f6285e6cd7d0fc7e0
reginaarmz <a375925@alumnos.uaslp.mx>
2024-09-20 10:53:02 -0600

    Update dictionary.c

--- dictionary.c
+++ dictionary.c
@@ -3,12 +3,28 @@
 void initializeDataDictionary(const char *dictionary) {
 
     long mainHeader= EMPTY_POINTE;
+    int stringToSave = 0;
     
     printf("Initializing Data Dictionary...\n");
     FILE *dictionary = fopen(dictionary, " ");
 
     fwrite(&mainHeader, sizeof(mainHeader), 1, dictionary);
-    fseek(dictionary, 50, SEEK_SET);
-    fwrite(&mainHeader, sizeof(mainHeader), 1, dictionary);
+
+    printf("How many strings would you like?");
+    scanf("%d", &stringToSave);
+
+    for(int counter=0; counter<stringToSave; counter++)
+    {
+        NODE currentNode;
+
+        currentNode. next = EMPTY_POINTER;
+
+        printf("Enter a value for a node #%d: ", counter);
+        scanf("^[\n]", currentNode.value);
+        fgets(&(currentNode. value), sizeof(currentNode.value), dictionary);
+        
+    }
+
+    return EXIT_SUCESS;
     
 }
